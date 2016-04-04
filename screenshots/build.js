0ba77d8a5d8b14237ebbb05b5a936710324e1688
Noel Stonehouse <Noel.Stonehouse@D2L.com>
2016-04-04 09:52:44 -0400

    US68374: Remove SASS Breadcrumbs. Update package.json scripts. Note: screenshot is not being captured properly yet.

--- screenshots/build.js
+++ screenshots/build.js
@@ -3,23 +3,25 @@ page.viewportSize = {width: 750, height: 1000};
 
 function capture(url, name, callback) {
 	page.open(url, function() {
-		var clipRect = page.evaluate(function (n) {
-			return document.querySelector('.screenshot-' + n)
-				.getBoundingClientRect();
-		}, name);
+		setTimeout(function() {
+			var clipRect = page.evaluate(function (n) {
+				return document.querySelector('.screenshot-' + n)
+					.getBoundingClientRect();
+			}, name);
 
-		page.clipRect = {
-			top: clipRect.top,
-			left: clipRect.left,
-			width: clipRect.width,
-			height: clipRect.height
-		};
-		page.render('./screenshots/' + name + '.png');
-		callback();
+			page.clipRect = {
+				top: clipRect.top,
+				left: clipRect.left,
+				width: clipRect.width,
+				height: clipRect.height
+			};
+			page.render('./screenshots/' + name + '.png');
+			callback();
+		}, 1000);
 	});
 }
 
-capture('./test/test.html', 'sample', phantom.exit);
+capture('./test/breadcrumbs.html', 'sample', phantom.exit);
 
 
 
